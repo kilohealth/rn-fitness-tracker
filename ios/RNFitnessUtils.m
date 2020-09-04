a26f466e5538bf1c9cf2eada706b0a59a1793b53
Lukas Baranauskas <lukas.baranauskas@kilo.health>
2020-09-04 14:57:27 +0300

    fix: getEndOfDay to return today 23:59:59 not 00:00:00 of next day

--- ios/RNFitnessUtils.m
+++ ios/RNFitnessUtils.m
@@ -15,12 +15,11 @@
 }
 
 +(NSDate *)endOfDay:(NSDate *)date {
-    NSDate *nextDay = [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitDay value:1 toDate:date options:0];
     NSCalendar *cal = [NSCalendar currentCalendar];
-    NSDateComponents *components = [cal components:( NSCalendarUnitDay| NSCalendarUnitMonth | NSCalendarUnitYear | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond ) fromDate:nextDay];
-    [components setHour:0];
-    [components setMinute:0];
-    [components setSecond:0];
+    NSDateComponents *components = [cal components:( NSCalendarUnitDay| NSCalendarUnitMonth | NSCalendarUnitYear | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond ) fromDate:date];
+    [components setHour:23];
+    [components setMinute:59];
+    [components setSecond:59];
     [components setMonth: components.month];
     [components setDay: components.day];
     [components setYear: components.year];
