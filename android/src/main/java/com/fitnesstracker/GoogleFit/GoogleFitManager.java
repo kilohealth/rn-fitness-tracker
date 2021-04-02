50b9c065e7b1393998370e002784d504d575f97a
Lukas Baranauskas <lukas.baranauskas@kilo.health>
2021-04-02 16:32:33 +0300

    fix: Check if historyClient exists before invoking it's methods

--- android/src/main/java/com/fitnesstracker/GoogleFit/GoogleFitManager.java
+++ android/src/main/java/com/fitnesstracker/GoogleFit/GoogleFitManager.java
@@ -2,6 +2,7 @@ package com.fitnesstracker.GoogleFit;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
@@ -17,140 +18,173 @@ import java.util.Date;
 
 public class GoogleFitManager implements ActivityEventListener {
 
-    private int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 111;
-    private int SIGN_IN_REQUEST_CODE = 112;
+  private int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 111;
+  private int SIGN_IN_REQUEST_CODE = 112;
 
-    private HistoryClient historyClient;
-    private RecordingService recordingService;
-    private Activity activity;
-    private ReactApplicationContext reactContext;
-    private Promise authorisationPromise;
+  private HistoryClient historyClient;
+  private RecordingService recordingService;
+  private Activity activity;
+  private ReactApplicationContext reactContext;
+  private Promise authorisationPromise;
 
-    FitnessOptions fitnessOptions = FitnessOptions.builder()
-            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-            .addDataType(DataType.TYPE_DISTANCE_DELTA, FitnessOptions.ACCESS_READ)
-            .addDataType(DataType.AGGREGATE_DISTANCE_DELTA, FitnessOptions.ACCESS_READ)
-            .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-            .build();
+  final Exception UnauthorizedEx = new Exception("Unauthorized");
 
-    public GoogleFitManager(ReactApplicationContext reactContext) {
-        this.reactContext = reactContext;
-        reactContext.addActivityEventListener(this);
+  FitnessOptions fitnessOptions = FitnessOptions.builder()
+    .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+    .addDataType(DataType.TYPE_DISTANCE_DELTA, FitnessOptions.ACCESS_READ)
+    .addDataType(DataType.AGGREGATE_DISTANCE_DELTA, FitnessOptions.ACCESS_READ)
+    .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+    .build();
+
+  public GoogleFitManager(ReactApplicationContext reactContext) {
+    this.reactContext = reactContext;
+    reactContext.addActivityEventListener(this);
+  }
+
+  public void subscribeToActivityData() {
+    Fitness.getRecordingClient(this.activity, GoogleSignIn.getLastSignedInAccount(this.activity))
+      .subscribe(DataType.TYPE_STEP_COUNT_DELTA);
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+
+  }
+
+  @Override
+  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+    if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+      if (resultCode == Activity.RESULT_OK) {
+        subscribeToActivityData();
+        accessGoogleFit();
+      } else {
+        this.authorisationPromise.resolve(false);
+      }
+    } else if (requestCode == SIGN_IN_REQUEST_CODE) {
+      GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(activity);
+      this.requestFitnessPermissions(googleSignInAccount);
     }
+  }
 
-    public void subscribeToActivityData() {
-        Fitness.getRecordingClient(this.activity, GoogleSignIn.getLastSignedInAccount(this.activity))
-                .subscribe(DataType.TYPE_STEP_COUNT_DELTA);
+  public void authorize(Promise promise, Activity activity) {
+    try {
+      this.activity = activity;
+      this.authorisationPromise = promise;
+
+      /* Check if app has google fit permissions */
+      if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this.reactContext), this.fitnessOptions)) {
+        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this.reactContext);
+        requestFitnessPermissions(googleSignInAccount);
+      } else {
+        accessGoogleFit();
+      }
+    } catch (Exception e) {
+      authorisationPromise.reject(e);
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void onNewIntent(Intent intent) {
+  public void isTrackingAvailable(Promise promise, Activity activity) {
+    try {
+      this.activity = activity;
+      this.authorisationPromise = promise;
 
+      /* Check if app has google fit permissions */
+      if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this.reactContext), this.fitnessOptions)) {
+        authorisationPromise.resolve(false);
+      } else {
+        authorisationPromise.resolve(true);
+      }
+    } catch (Exception e) {
+      authorisationPromise.reject(e);
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                subscribeToActivityData();
-                accessGoogleFit();
-            } else {
-                this.authorisationPromise.resolve(false);
-            }
-        } else if (requestCode == SIGN_IN_REQUEST_CODE) {
-            GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(activity);
-            this.requestFitnessPermissions(googleSignInAccount);
-        }
+
+  private void requestFitnessPermissions(GoogleSignInAccount googleSignInAccount) {
+    GoogleSignIn.requestPermissions(
+      activity,
+      GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+      googleSignInAccount,
+      fitnessOptions
+    );
+  }
+
+  private void accessGoogleFit() {
+    try {
+      this.recordingService = new RecordingService(activity);
+      this.recordingService.subscribe();
+      this.historyClient = new HistoryClient(activity);
+      authorisationPromise.resolve(true);
+    } catch (Exception e) {
+      authorisationPromise.reject(e);
+      e.printStackTrace();
     }
+  }
 
-    public void authorize(Promise promise, Activity activity) {
-        try {
-            this.activity = activity;
-            this.authorisationPromise = promise;
-
-            /* Check if app has google fit permissions */
-            if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this.reactContext), this.fitnessOptions)) {
-                GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this.reactContext);
-                requestFitnessPermissions(googleSignInAccount);
-            } else {
-                accessGoogleFit();
-            }
-        } catch (Exception e) {
-            authorisationPromise.reject(e);
-            e.printStackTrace();
-        }
+  public void getStepsToday(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getStepsToday(promise);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    public void isTrackingAvailable(Promise promise, Activity activity) {
-        try {
-            this.activity = activity;
-            this.authorisationPromise = promise;
-
-            /* Check if app has google fit permissions */
-            if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this.reactContext), this.fitnessOptions)) {
-                authorisationPromise.resolve(false);
-            } else {
-                authorisationPromise.resolve(true);
-            }
-        } catch (Exception e) {
-            authorisationPromise.reject(e);
-            e.printStackTrace();
-        }
+  public void getStepsWeekTotal(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getWeekData(promise, 0);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-
-    private void requestFitnessPermissions(GoogleSignInAccount googleSignInAccount) {
-        GoogleSignIn.requestPermissions(
-                activity,
-                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
-                googleSignInAccount,
-                fitnessOptions
-        );
+  public void getStepsDaily(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getStepsDaily(new Date(), Arguments.createMap(), 0, promise);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    private void accessGoogleFit() {
-        try {
-            this.recordingService = new RecordingService(activity);
-            this.recordingService.subscribe();
-            this.historyClient = new HistoryClient(activity);
-            authorisationPromise.resolve(true);
-        } catch (Exception e) {
-            authorisationPromise.reject(e);
-            e.printStackTrace();
-        }
+  public void getDistanceToday(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getDistanceToday(promise);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    public void getStepsToday(final Promise promise) {
-        this.historyClient.getStepsToday(promise);
+  public void getDistanceWeekTotal(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getWeekData(promise, 1);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    public void getStepsWeekTotal(final Promise promise) {
-        this.historyClient.getWeekData(promise, 0);
-    }
-
-    public void getStepsDaily(final Promise promise) {
-        this.historyClient.getStepsDaily(new Date(), Arguments.createMap(), 0, promise);
-    }
-
-    public void getDistanceToday(final Promise promise) {
-        this.historyClient.getDistanceToday(promise);
-    }
-
-    public void getDistanceWeekTotal(final Promise promise) {
-        this.historyClient.getWeekData(promise, 1);
-    }
-
-    public void queryStepsTotal(final Promise promise, long startTime, long endTime) {
+  public void queryStepsTotal(final Promise promise, long startTime, long endTime) {
+    if (this.historyClient != null) {
       this.historyClient.getTotalForTimeRange(promise, startTime, endTime, 0);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    public void queryDistanceTotal(final Promise promise, long startTime, long endTime) {
+  public void queryDistanceTotal(final Promise promise, long startTime, long endTime) {
+    if (this.historyClient != null) {
       this.historyClient.getTotalForTimeRange(promise, startTime, endTime, 1);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
-    public void getDistanceDaily(final Promise promise) {
-        this.historyClient.getDistanceDaily(new Date(), Arguments.createMap(), 0, promise);
-
+  public void getDistanceDaily(final Promise promise) {
+    if (this.historyClient != null) {
+      this.historyClient.getDistanceDaily(new Date(), Arguments.createMap(), 0, promise);
+    } else {
+      promise.reject(UnauthorizedEx);
     }
+  }
 
 }
