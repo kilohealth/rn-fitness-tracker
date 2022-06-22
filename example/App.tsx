b70506b9091f309b46cadf5f4fba5b18de2b3e13
Matas <matas.dziadaravicius@kilo.health>
2022-06-22 15:17:37 +0300

    feat(example): added authorize button

--- example/App.tsx
+++ example/App.tsx
@@ -1,93 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  AuthorizationPermissions,
+  FitnessTracker,
+  GoogleFitDataTypes,
+  HealthDataType,
+} from '@kilohealth/rn-fitness-tracker';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const permissions: AuthorizationPermissions = {
+  healthReadPermissions: [HealthDataType.StepCount],
+  healthWritePermissions: [HealthDataType.StepCount],
+  googleFitReadPermissions: [GoogleFitDataTypes.Steps],
+  googleFitWritePermissions: [GoogleFitDataTypes.Steps],
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [authorized, setAuthorized] = useState(false);
+  const isAuthorized: string = authorized ? 'Authorized' : 'Not authorized';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const authorize = useCallback(async () => {
+    await FitnessTracker.authorize(permissions);
+
+    setAuthorized(true);
+  }, [setAuthorized]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Text testID="hello_button">Should be here!</Text>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={styles.container}>
+          <Text>Status: {isAuthorized}</Text>
+          <Button title="Authorize" onPress={authorize} />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -95,21 +47,8 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    padding: 32,
   },
 });
 
