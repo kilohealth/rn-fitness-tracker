10a30b3bdb650b73602d34abd1a493c2f8edbcbf
Matas <matas.dziadaravicius@kilo.health>
2022-07-05 13:38:27 +0300

    feat(example): write, get, delete height and weight data.

--- example/App.tsx
+++ example/App.tsx
@@ -33,12 +33,12 @@ const startOfToday = +startOfDay(new Date());
 const mockData: Array<HealthKitWriteData> = [
   {
     key: HealthDataType.StepCount,
-    unit: UnitType.count,
+    unit: UnitType.Count,
     amount: 100,
   },
   {
     key: HealthDataType.StepCount,
-    unit: UnitType.count,
+    unit: UnitType.Count,
     amount: 100,
     metadata: {
       HKMetadataKeyWasUserEntered: true,
@@ -46,13 +46,13 @@ const mockData: Array<HealthKitWriteData> = [
   },
   {
     key: HealthDataType.StepCount,
-    unit: UnitType.count,
+    unit: UnitType.Count,
     amount: 100,
     timestamp: startOfToday,
   },
   {
     key: HealthDataType.StepCount,
-    unit: UnitType.count,
+    unit: UnitType.Count,
     amount: 100,
     timestamp: startOfToday,
     metadata: {
@@ -64,9 +64,11 @@ const mockData: Array<HealthKitWriteData> = [
 const App = () => {
   const [authorized, setAuthorized] = useState(false);
   const isAuthorized: string = authorized ? 'Authorized' : 'Not authorized';
-  const [stepsToday, setStepsToday] = useState<number | null>(null);
+  const [stepsToday, setStepsToday] = useState<number | undefined>(undefined);
   const [stepsTodayWritten, setStepsTodayWritten] = useState(false);
   const [stepsDeleted, setStepsDeleted] = useState(false);
+  const [height, setHeight] = useState<number | undefined>(undefined);
+  const [weight, setWeight] = useState<number | undefined>(undefined);
 
   const authorize = useCallback(async () => {
     await FitnessTracker.authorize(permissions);
@@ -123,12 +125,67 @@ const App = () => {
     }
   }, []);
 
+  const getHeightAndWeight = useCallback(async () => {
+    try {
+      const h = await FitnessTracker.getLatestHeightRecord();
+      const w = await FitnessTracker.getLatestWeightRecord();
+
+      setHeight(h?.quantity);
+      setWeight(w?.quantity);
+
+      console.log('Success');
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const writeHeightAndWeight = useCallback(async () => {
+    try {
+      await HealthKit.writeData({
+        key: HealthDataType.Height,
+        unit: UnitType.Meters,
+        amount: 1.52,
+      });
+      await HealthKit.writeData({
+        key: HealthDataType.BodyMass,
+        unit: UnitType.Kilograms,
+        amount: 99.9,
+      });
+
+      console.log('Success');
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const deleteHeightAndWeight = useCallback(async () => {
+    try {
+      await HealthKit.deleteRecord({
+        key: HealthDataType.Height,
+        startDate: startOfToday,
+        endDate: +new Date(),
+      });
+      await HealthKit.deleteRecord({
+        key: HealthDataType.BodyMass,
+        startDate: startOfToday,
+        endDate: +new Date(),
+      });
+
+      console.log('Success');
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View style={styles.container}>
           <Text testID="authorization_status">Status: {isAuthorized}</Text>
+          <Text testID="height">Height: {height}</Text>
+          <Text testID="weight">Weight: {weight}</Text>
+
           <Button
             title="Authorize"
             onPress={authorize}
@@ -152,12 +209,28 @@ const App = () => {
             <Text testID="data_was_deleted">Data deleted successfully.</Text>
           )}
           <Button
-            title="Write steps mock data."
+            title="Delete steps mock data."
             onPress={deleteStepsData}
             testID="delete_steps_button"
           />
 
           <Button title="Get latest workout." onPress={getWorkoutToday} />
+
+          <Button
+            title="Get Height and Weight."
+            onPress={getHeightAndWeight}
+            testID="getHeightAndWeightButton"
+          />
+          <Button
+            title="Write Height and Weight."
+            onPress={writeHeightAndWeight}
+            testID="writeHeightAndWeightButton"
+          />
+          <Button
+            title="Delete Height and Weight."
+            onPress={deleteHeightAndWeight}
+            testID="deleteHeightAndWeightButton"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
